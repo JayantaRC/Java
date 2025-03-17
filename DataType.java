8b79812aaf1ba693da7fc484c199f4a979f734c3
JayantaRC <omjayantaom@gmail.com>
2025-03-17 16:39:52 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,31 @@
+
+package com.mycompany.javatutorial;
+
+
+public class DataType {
+    
+    public static void main (String[] args) {
+        
+        //Dynamic Initialisation
+        
+        boolean b = true;
+        char c = 'a';
+        short s = 2345;
+        int i = 4566;
+        float f = 45.9f;
+        double d = 34.56;
+        
+        
+        
+        System.out.println("boolean : "+b);
+        System.out.println("Character : "+c);
+        System.out.println("short : "+s);
+        System.out.println("integer : "+i);
+        System.out.println("Float : "+f);
+        System.out.println("Double : "+d);
+        
+       
+        
+    }
+    
+}
