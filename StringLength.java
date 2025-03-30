d7ca35d4bf6dc30f158e0498cf6352b53f9e70c0
JayantaRC <omjayantaom@gmail.com>
2025-03-30 16:48:19 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.javatutorial;
+
+
+public class StringLength {
+    
+    public static void main (String[]args){
+        
+       String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        
+       //It will count the total length of Alphabet
+       System.out.println("The Length of the Alphabet is : "+alphabet.length());
+       
+       //The Output is 26
+    }
+    
+}
