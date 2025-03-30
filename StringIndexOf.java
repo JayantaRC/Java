aa2a1257d0b81bd87baef832e3723c39ac17e6ad
JayantaRC <omjayantaom@gmail.com>
2025-03-30 16:49:09 +0530

    Add files via upload

---
+++ StringIndexOf.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.javatutorial;
+
+
+public class StringIndexOf {
+    
+    public static void main (String[]args){
+        
+        
+        /* This method returns the index (the position) 
+           of the first occurrence of a specified text 
+           in a string (including whitespace) */
+        
+        String text = "My organisation is great";
+        System.out.println("Find where the 'organisation' occurs! : "+text.indexOf("organisation"));
+    }
+    
+}
