8ac4ad62eb38beb6b194be8c5ac911b6c6dfdd95
JayantaRC <omjayantaom@gmail.com>
2025-03-30 16:48:41 +0530

    Add files via upload

---
+++ StringUpperLower.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.javatutorial;
+
+
+public class StringUpperLower {
+    
+    public static void main (String[]args){
+        
+        //The Code will help to make "Hello, India!" to Cap "HELLO, INDIA!"
+        String upper = "Hello, India!";
+        System.out.println("Capital : "+upper.toUpperCase());
+        
+        
+        //The Code will help to make "Hello, India!" to Small "hello, india!"
+        String lower = "Hello, India!";
+        System.out.println("Small : "+lower.toLowerCase());
+    }
+    
+}
