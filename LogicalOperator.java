1b23be5126df341c4244e115c335dff72ffc3c6f
JayantaRC <omjayantaom@gmail.com>
2025-04-07 11:34:39 +0530

    Add files via upload

---
+++ LogicalOperator.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.javatutorial;
+import java.util.Scanner;
+
+public class LogicalOperator {
+    
+    public static void main (String[]args){
+        
+        Scanner input = new Scanner (System.in);
+        
+        char letter;
+        System.out.print("Enter any single character to find Vower or Consonant : ");
+        letter = input.next().charAt(0);
+        
+        //This Logical Operator progarm is ready to help to find the vowel or consonant with the help of Control Statement.
+        
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U'){
+            System.out.println("Vowel");
+        }
+        
+        else{
+        System.out.println("Consonant");
+    }
+        
+    }
+    
+}
