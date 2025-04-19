d6f178834434b53d3d492b4ccfb9eb9c52fb2a16
JayantaRC <omjayantaom@gmail.com>
2025-04-19 14:06:03 +0530

    Add files via upload

---
+++ ConditionalTernary.java
@@ -0,0 +1,31 @@
+
+package com.mycompany.javatutorial;
+
+import java.util.Scanner;
+public class ConditionalTernary {
+    
+    public static void main (String[]args){
+        
+        Scanner input = new Scanner (System.in);
+        int x, y;
+          // Prompt user to enter the first number
+        System.out.print("Enter First digit : ");
+        x = input.nextInt();
+        
+          // Prompt user to enter the second number
+        System.out.print("Enter Second digit : ");
+        y = input.nextInt();
+        
+        // Check if both numbers are equal
+        if (x==y){
+            System.out.println("Both numbers are equal.");
+        }
+        else{
+             // Use ternary operator to find the larger number between x and y
+            int devil = (x>y) ? x : y;
+            System.out.println("Large Number : "+devil);   // Print the larger number
+        }
+        
+    }
+    
+}
