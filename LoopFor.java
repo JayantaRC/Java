2059e2db8829bfed248bf2d7c586314736cffee6
JayantaRC <omjayantaom@gmail.com>
2025-05-26 14:57:11 +0530

    Add files via upload

---
+++ LoopFor.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.javatutorial;
+
+
+public class LoopFor {
+    
+    public static void main (String[]args){
+        
+        int number = 2;
+        for(int i = 1; i<=10; i++){
+            
+             // Print the multiplication table for the number 2
+            System.out.println(number + "x" + i + "=" + (number * i));
+        }
+        
+    }
+    
+}
