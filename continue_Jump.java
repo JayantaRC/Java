c13ad93b695b45409a4a7d5c451fc87cf3c70754
JayantaRC <omjayantaom@gmail.com>
2025-05-31 20:17:24 +0530

    Add files via upload

--- continue_Jump.java
+++ continue_Jump.java
@@ -30,6 +30,24 @@ public class continue_Jump {
              // Print values like 1, 4, 7, 10 (skipped), 13, 16, ... 100
             System.out.println(i);
         }
+        // Print a separator line
+        System.out.println("_______________");
+        
+        // Loop from i = 1 to i <= 100, incrementing i by 3 on each iteration
+        for (int i = 1; i<=100; i=i+3){
+            
+            // If i is equal to 10, skip the rest of the loop and continue with the next iteration
+            if(i==10){
+                continue;
+            }
+            
+             // If i becomes greater than 13, exit the loop entirely
+            if(i>13){
+                break;
+            }
+            // Print the value of i to the console
+            System.out.println(i);
+        }
     }
     }
     
