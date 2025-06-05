5fc6061aa4ea5afe5b394e7ea67c6a633e1052bc
JayantaRC <omjayantaom@gmail.com>
2025-06-05 17:10:30 +0530

    Update Array_II.java

--- Array_II.java
+++ Array_II.java
@@ -26,12 +26,31 @@ public class Array_II {
             sum = sum + number[i];  // Add the current number to the total sum
         }
 
-        System.out.println("Total : " + sum);
-
         double avg = sum / number.length;  // Calculate the average (sum divided by 5)
+
+        System.out.println("Total : " + sum);
 
         System.out.println("Average : " + avg);
 
+        // Assume the first number in the array is the maximum and minimum initially
+        double max = number[0];  // Starting max value
+        double min = number[0];  // Starting min value
+
+        // Loop through the rest of the array (from index 1 to 4)
+        for (int i = 1; i < 5; i++) {
+
+            // If current number is greater than current max, update max
+            if (max < number[i]) {
+                max = number[i]; // Store the new maximum value
+            }
+            // If current number is less than current min, update min
+            if (min > number[i]) {
+                min = number[i]; // Store the new minimum value
+            }
+        }
+        // After the loop ends, print the maximum and minimum values found
+        System.out.println("Maximum : " + max);
+        System.out.println("Minimum : " + min);
     }
 
 }
