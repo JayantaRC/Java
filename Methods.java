f15cc6b8244f162c12b1c0ba2da1a113a56099db
JayantaRC <omjayantaom@gmail.com>
2025-12-13 22:01:40 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.basicjava;
+
+
+public class Methods {
+    
+    public static void greet () {
+        System.out.println("Hello");
+        System.out.println("Good to see you are doing Java");
+        System.out.println("Have a nice day");
+    }
+    
+    public static void main (String [] args) {
+        greet();
+    }
+}
+
+// main() method can start from anywhere,
+/* Here we used void method and main() method as well, 
+For example main () method is like a swith and other methods like light bulb.
+*/
