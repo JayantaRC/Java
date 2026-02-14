bf479ed0a7450e382b140bef5a203f2033883787
JayantaRC <omjayantaom@gmail.com>
2026-02-14 22:58:03 +0530

    Add files via upload

---
+++ PrintFactors.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.theforloop;
+
+
+public class PrintFactors {
+    
+    public static void printFactors (int number){
+
+if (number < 1){
+System.out.println("Invalid Value");
+return;
+}
+
+for (int i = 1; i <= number; i++){
+
+if ( number % i == 0){
+System.out.println(i);
+}
+}
+}
+
+public static void main (String [] args){
+
+    printFactors (114);
+}
+    
+}
