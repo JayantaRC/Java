20442ad074313bf5d9559cee198878d8d7831d41
JayantaRC <omjayantaom@gmail.com>
2026-03-04 22:01:31 +0530

    Add files via upload

---
+++ DebugDemo.java
@@ -0,0 +1,34 @@
+
+package com.mycompany.theforloop;
+
+
+public class DebugDemo {
+    
+    public static int getLargestPrime (int number){
+
+if (number < 1){
+return -1;
+}
+
+int factor = -1;
+for (int i = 2; i * i <= number; i++){
+
+if (number % i != 0){
+continue;
+}
+
+factor = i;
+while (number % i == 0){
+number = number / i;
+}
+}
+
+return number == 1 ? factor : number;
+}
+
+public static void main (String [] args){
+    
+    System.out.println(getLargestPrime(21));
+}
+    
+}
