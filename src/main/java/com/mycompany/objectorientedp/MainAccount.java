c4397cbf8124dbbcd00a0dad5f82923c990fb66d
JayantaRC <omjayantaom@gmail.com>
2026-04-18 21:55:04 +0530

    MainAccount.java

--- src/main/java/com/mycompany/objectorientedp/MainAccount.java
+++ src/main/java/com/mycompany/objectorientedp/MainAccount.java
@@ -6,10 +6,20 @@ public class MainAccount {
     
     public static void main(String [] args){
         
-        Account bobsAccount = new Account ();
+        Account bobsAccount = new Account ("1122334455", 1000.00, "Bob Brave",
+        "email@bob.com", "32751 45797");
+        
+
+        System.out.println(bobsAccount.getNumber());
+        System.out.println(bobsAccount.getBalance());
+        System.out.println(bobsAccount.getCustomerName());
+        System.out.println(bobsAccount.getCustomerEmail());
+        System.out.println(bobsAccount.getCustomerPhone());
+        
         bobsAccount.withdrawFunds(100.0);
         bobsAccount.depositFunds(250);
         bobsAccount.withdrawFunds(50);
+        bobsAccount.withdrawFunds(500);
         
     }
     
